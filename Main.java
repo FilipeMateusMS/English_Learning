aa3228a5b6a419e322b8006474e7cd33c71cd636
Filipe Mateus <146008498+FilipeMateusMS@users.noreply.github.com>
2024-02-23 17:28:35 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main
 {
 	public static void main(String[] args) {
 		// Antes de rodar o sistema, configure o arquivo com/recursos/workspaces.properties
+		// Existe um arquivo .csv válido com 5000 palavras em English_Learning/recursos/Dados/Dados.csv
 		TelaPrincipal.main(null);
 	}
 }
